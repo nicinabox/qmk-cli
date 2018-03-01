38abea6f93c72da759c73dffa0b2773406e287ac
Nic Aitch <nic@nicinabox.com>
2018-02-28 19:25:18 -0600

    Add makefile wrapper to dump values from target makefile

---
+++ lib/makefile.rb
@@ -0,0 +1,40 @@
+require 'tempfile'
+
+class Makefile
+  def initialize(include_makefile)
+    @include_makefile = include_makefile
+    @file = Tempfile.new('makefile')
+
+    write
+  end
+
+  def write
+    @file.write contents
+    @file.close
+  end
+
+  def run(variable)
+    `make -f #{@file.path} -f #{@include_makefile} print-#{variable.upcase}`
+  end
+
+  def value(variable)
+    output = run variable
+    _, value = parse output
+    value
+  end
+
+  def parse(output)
+    match = output.match /^(\w+)=(.+)/
+    if match
+      [match[1], match[2]]
+    end
+  end
+
+  def contents
+    <<-contents
+.PHONY: print-%
+print-%:
+\t@echo '$*=$($*)'
+    contents
+  end
+end
